f4a8af9acf6743226d00e6c6e17795206f9ec30a
Nipun Mittal <nipunmittal15@gmail.com>
2021-06-25 09:43:29 +0530

    delete Song method added

--- ShufflePlaylist.cpp
+++ ShufflePlaylist.cpp
@@ -13,6 +13,8 @@ map<string, list<string>::iterator> songIterator;
 list<string>::iterator currSongIndex;
 
 public:
+
+    // this method is used to add new Song in the playlist
     string addSong(string name) {
         if(songIterator.find(name) != songIterator.end()) {
             return "Song already present!!";
@@ -26,6 +28,7 @@ public:
         return name + " added!";
     }
 
+    // this method is used to play the next Song in the playlist
     string getNextSong() {
         if(playlist.size() == 0) {
             return "No Song present!!";
@@ -38,6 +41,7 @@ public:
         return name;
     }
 
+    // this method is used to play a particular Song in the playlist
     string playSong(string name) {
         if(songIterator.find(name) == songIterator.end()) {
             return "Song not present!!";
@@ -48,6 +52,22 @@ public:
         playlist.erase(songIterator[name]);
         songIterator[name] = playlist.insert(currSongIndex, name);
         return name;
+    }
+
+    // this method is used to delete a Song in the playlist
+    string deleteSong(string name) {
+        if(songIterator.find(name) == songIterator.end()) {
+            return "Song not present!!";
+        }
+        if(currSongIndex == songIterator[name]) {
+            currSongIndex = playlist.erase(currSongIndex);
+            if(currSongIndex == playlist.end())
+                currSongIndex = playlist.begin();
+        } else {
+            playlist.erase(songIterator[name]);
+        }
+        songIterator.erase(name);
+        return name + " deleted!";
     }
 };
 
@@ -63,9 +83,12 @@ int main()
             cout << playlist.addSong(input) << endl;
         } else if(option == 2) {
             cout << playlist.getNextSong() << endl;
-        } else {
+        } else if(option == 3) {
             cin >> input;
             cout << playlist.playSong(input) << endl;
+        } else {
+            cin >> input;
+            cout << playlist.deleteSong(input) << endl;
         }
     }
     return 0;
